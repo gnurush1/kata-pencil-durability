58392ba3b96140c76cec5469ea9372230bd5e0c9
gnurushe <gnurushe@umich.edu>
2019-07-05 14:34:21 -0400

    Added test case for not being able to finish a sentence becuase the pencil ran out of durability.

--- test/PencilDurabilityTest.py
+++ test/PencilDurabilityTest.py
@@ -1,20 +1,43 @@
 from sys import argv
 
 def main():
-    test1()
-
+	testWriteFunctionality()
+	testPencilDegradationWhereWeCantFinishASentence()
 
 def testWriteFunctionality():
-    correctOutput = "She sells sea shells down by the sea shore"
-    pencilDurability = PencilDurability()
-    currentSentenceOnPaper = "She sells sea shells"
-    sentenceToWrite = " down by the sea shore"
+	correctOutput = "She sells sea shells down by the sea shore"
+	pencil = Pencil()
+	currentSentenceOnPaper = "She sells sea shells"
+	sentenceToWrite = " down by the sea shore"
 
-    assert correctOutput == pencilDurability.write(currentSentenceOnPaper, sentenceToWrite)
+	assert correctOutput == pencil.write(currentSentenceOnPaper, sentenceToWrite)
 
-class PencilDurability():
-    def write(self, currentSentenceOnPaper, sentenceToWrite):
-        return currentSentenceOnPaper + sentenceToWrite
+def testPencilDegradationWhereWeCantFinishASentence():
+	correctOutput = "She sells sea shells down by the sea      "
+	durability = 12
+	pencil = Pencil(durability)
+	currentSentenceOnPaper = "She sells sea shells"
+	sentenceToWrite = " down by the sea shore"
+	
+	assert correctOutput == pencil.write(currentSentenceOnPaper, sentenceToWrite)
+
+class Pencil():
+	def __init__(self, durability = 200):
+		self.durability = durability
+
+	def write(self, currentSentenceOnPaper, sentenceToWrite):
+		sentenceToWriteBuffer = ""
+		for i in range(0, len(sentenceToWrite)):
+			character = sentenceToWrite[i]
+			if not character.isspace():
+				if character.isupper() and self.durability >= 2:
+					self.durability -= 2
+				elif character.islower() and self.durability >= 1:
+					self.durability -= 1
+				else:
+					character = " "
+			sentenceToWriteBuffer += character
+		return currentSentenceOnPaper + sentenceToWriteBuffer
 
 if __name__ == '__main__':
 	main()
