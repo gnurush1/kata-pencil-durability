46e720679a85dac9af45762d9447809ffed6ee2a
gnurushe <gnurushe@umich.edu>
2019-07-05 14:51:47 -0400

    Added test case for successfuly sharpening pencil after running out of durability.

--- test/PencilDurabilityTest.py
+++ test/PencilDurabilityTest.py
@@ -3,6 +3,7 @@ from sys import argv
 def main():
 	testWriteFunctionality()
 	testPencilDegradationWhereWeCantFinishASentence()
+	testSharpenPencilAfterRunningOutOfDurabilityAndThenFinishingTheSentence()
 
 def testWriteFunctionality():
 	correctOutput = "She sells sea shells down by the sea shore"
@@ -18,12 +19,33 @@ def testPencilDegradationWhereWeCantFinishASentence():
 	pencil = Pencil(durability)
 	currentSentenceOnPaper = "She sells sea shells"
 	sentenceToWrite = " down by the sea shore"
-	
+
 	assert correctOutput == pencil.write(currentSentenceOnPaper, sentenceToWrite)
 
+def testSharpenPencilAfterRunningOutOfDurabilityAndThenFinishingTheSentence():
+	correctOutput = "She sells sea shells down by the sea "
+	durability = 12
+	length = 2
+	pencil = Pencil(durability, length)
+	currentSentenceOnPaper = "She sells sea shells"
+	sentenceToWrite = " down by the sea "
+
+	writingOutput = pencil.write(currentSentenceOnPaper, sentenceToWrite)
+	assert correctOutput == writingOutput
+	assert pencil.durability == 0
+
+	pencil.sharpen()
+	assert pencil.durability == 12
+
+	correctOutput = "She sells sea shells down by the sea shore"
+	wordToFinishSentence = "shore"
+	assert correctOutput == pencil.write(writingOutput, wordToFinishSentence)
+
 class Pencil():
-	def __init__(self, durability = 200):
+	def __init__(self, durability = 200, length = 5):
+		self.maxDurability = durability
 		self.durability = durability
+		self.length = length
 
 	def write(self, currentSentenceOnPaper, sentenceToWrite):
 		sentenceToWriteBuffer = ""
@@ -38,6 +60,11 @@ class Pencil():
 					character = " "
 			sentenceToWriteBuffer += character
 		return currentSentenceOnPaper + sentenceToWriteBuffer
+
+	def sharpen(self):
+		if self.length != 0:
+			self.durability = self.maxDurability
+			self.length -= 1
 
 if __name__ == '__main__':
 	main()
